f0f7762a3641cbe351d638e9cb49c143c54795bd
ASPCoding <152766272+ASPCoding@users.noreply.github.com>
2026-02-28 15:49:48 -0800

    Added Link to Editor

--- Frontend/src/components/Landing.tsx
+++ Frontend/src/components/Landing.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 function Landing() {
     return (
         <>
-            <div>Landing</div>
+            <div id="landing-wrapper">
+                <div id="text">
+                    <h1>Dream House Building,</h1>
+                    <h1>Powered by AI</h1>
+                    <Link to="/Editor" className="nav-button">
+                        Go to Editor
+                    </Link>
+
+                </div>
+            </div>
         </>
     );
 }
